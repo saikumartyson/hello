dda462b0f76834faad37ecba02f92ad6843a5cdf
saikumartyson <60337564+saikumartyson@users.noreply.github.com>
2020-01-27 16:32:44 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 public class Hello{
 public static void main(String s[])
 {
-int n=5,i;
+int n=5;
 
-for(i=1;i<=n;i++){System.out.println("Hello World");} 
+for(int i=1;i<=n;i++){System.out.println("Hello World");} 
 }
 
 }
